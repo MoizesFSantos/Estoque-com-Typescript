3ed06bd0c723b60981e2367fb5b34506dfbe56af
Moizes Ferreira dos Santos <77401158+MoizesFSantos@users.noreply.github.com>
2021-02-23 15:52:27 -0300

    primerio commit

---
+++ src/config/database.ts
@@ -0,0 +1,14 @@
+require('dotenv/config');
+
+module.exports ={
+    dialect: 'postegres',
+    host: 'localhost',
+    username:'moizesF',
+    password:'ordem66',
+    database:'estoque',
+    port: 5432,
+    define:{
+        timestamps: true,
+    },
+
+};
